2dae90fdb8beb00423f64c32556d8a72c91403d6
GUBBIB <moon.dragon250@gmail.com>
2025-12-01 22:38:57 +0900

    AuthResponseDTO 속성 순서 변경

--- backend/src/main/java/com/github/gubbib/backend/DTO/Auth/AuthResponseDTO.java
+++ backend/src/main/java/com/github/gubbib/backend/DTO/Auth/AuthResponseDTO.java
@@ -3,7 +3,7 @@ package com.github.gubbib.backend.DTO.Auth;
 public record AuthResponseDTO(
         Long userId,
         String email,
-        String refreshToken,
-        String accessToken
+        String accessToken,
+        String refreshToken
 ) {
 }
