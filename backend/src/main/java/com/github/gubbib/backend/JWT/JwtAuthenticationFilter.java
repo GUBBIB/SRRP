cb155ea09f6d5f09774ac92261cc008dc7d21979
GUBBIB <moon.dragon250@gmail.com>
2025-12-02 01:03:46 +0900

    JWT Authentication Filter Chain 추가

---
+++ backend/src/main/java/com/github/gubbib/backend/JWT/JwtAuthenticationFilter.java
@@ -0,0 +1,37 @@
+package com.github.gubbib.backend.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if(header !=null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+            try {
+                Jws<Claims> claims = jwtTokenProvider.parseToken(token);
+                Long userId = Long.parseLong(claims.getBody().getSubject());
+            } catch (Exception e){
+                // 추후 수정
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
