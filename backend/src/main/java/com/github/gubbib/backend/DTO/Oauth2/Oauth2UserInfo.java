dc9df1fd4ce76d5cb664c08b50a766c49196c0b2
GUBBIB <moon.dragon250@gmail.com>
2025-12-02 21:30:51 +0900

    OAuth2UserInfo - ofGoogle 메소드 추가

--- backend/src/main/java/com/github/gubbib/backend/DTO/Oauth2/Oauth2UserInfo.java
+++ backend/src/main/java/com/github/gubbib/backend/DTO/Oauth2/Oauth2UserInfo.java
@@ -1,11 +1,13 @@
 package com.github.gubbib.backend.DTO.Oauth2;
 
 import com.github.gubbib.backend.Exception.Auth.AuthProviderMismatchException;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Map;
 
+@Builder
 public record Oauth2UserInfo(
     String name,
     String email,
@@ -20,13 +22,17 @@ public record Oauth2UserInfo(
         };
     }
 
-    private static Map<String, Object> ofGoogle(Map<String, Object> attributes){
-
+    private static Oauth2UserInfo ofGoogle(Map<String, Object> attributes){
+        return Oauth2UserInfo.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .provider("GOOGLE")
+                .build();
     }
-    private static Map<String, Object> ofKakao(Map<String, Object> attributes){
-
+    private static Oauth2UserInfo ofKakao(Map<String, Object> attributes){
+        return null;
     }
-    private static Map<String, Object> ofNaver(Map<String, Object> attributes){
-
+    private static Oauth2UserInfo ofNaver(Map<String, Object> attributes){
+        return null;
     }
 }
