0f1f160a17172367b099b99cd4aa491d1a3d8bb5
GUBBIB <moon.dragon250@gmail.com>
2025-12-04 21:43:25 +0900

    어노테이션 RestController로 벽녕

--- backend/src/main/java/com/github/gubbib/backend/Controller/User/UserController.java
+++ backend/src/main/java/com/github/gubbib/backend/Controller/User/UserController.java
@@ -8,13 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @RequestMapping("/api/v1/users")
 public class UserController {
